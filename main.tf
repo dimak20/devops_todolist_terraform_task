290cf2bd3e00e76ccc857888ea600262171cd41c
Dmytro Kolchak <dima.kolhac@gmail.com>
2026-03-15 03:48:04 +0200

    fix

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
-data "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg" {
   name = var.resource_group_name
+  location = var.location
 }
 module "network_module" {
   source                      = "./modules/network"
