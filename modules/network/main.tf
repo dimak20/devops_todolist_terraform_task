e80966c22b47a9bdb3feea9eb15281dfd42bf1ca
Dmytro Kolchak <dima.kolhac@gmail.com>
2026-03-15 03:57:02 +0200

    fmt

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -48,7 +48,7 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-    security_rule {
+  security_rule {
     name                       = "API"
     priority                   = 1004
     direction                  = "Inbound"
--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,16 +1,16 @@
 variable "location" {
   description = "Location of the storage account"
-  type = string
+  type        = string
 }
 variable "resource_group_name" {
   description = "Name of the resource group"
-  type = string
+  type        = string
 }
 variable "storage_account_name" {
   description = "Name of the storage account"
-  type = string
+  type        = string
 }
 variable "storage_container_name" {
   description = "Name of the storage container"
-  type = string
+  type        = string
 }
